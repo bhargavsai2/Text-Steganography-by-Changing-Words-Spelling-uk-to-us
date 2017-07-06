656f691e041e278e7e9e3cecd881e28031de7b57
Bhargav Sai Gajula <bhargavsai554@gmail.com>
2017-07-06 13:05:14 -0400

    Add files via upload

--- Text Steganography/stego.java
+++ Text Steganography/stego.java
@@ -1,80 +1,94 @@
-
-/**
- * text Stego
- * 
- * @author bhargav 
- * @version 1.0
- */
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 import java.security.*;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.xml.bind.DatatypeConverter;
-import java.util.Base64;
+//import java.util.Base64;
 import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 
 /* Name of the class has to be "Main" only if the class is public. */
-public class stego extends JFrame implements ActionListener
+public class stego extends JFrame implements ActionListener 
 {
-    private String inpstr;
-  
-    JTextField t2;  JButton b; JLabel lb;
-      test(){
+     private static final String NULL = null;
+	//String inpstr="bhargav";
+    static String str="";
+    JTextField t2;  JButton b; JLabel lb;//lb1;
+      stego(){
     JFrame fr= new JFrame("Steganography");  
     
    lb=new JLabel("enter text to hide");
    lb.setBounds(50,20,100,30);
     t2=new JTextField();  
+    
+    
     t2.setBounds(50,50,200,30);  
     b=new JButton("Click Here");  
     b.setBounds(50,100,95,30);  
+   // lb1=new JLabel("Your files are generated");
+    //lb1.setBounds(50,200,200,30);
     fr.add(t2); fr.add(b);  fr.add(lb);
     fr.setSize(400,400);  
     fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     fr.setLayout(null);  
     fr.setVisible(true);  
+    b.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			try
+			{
+			str=t2.getText();
+			steganography_and_encryption();
+			//System.out.println(str);
+			}
+			catch(Exception e)
+			{
+			}
+			
+			
+		}
+	});
 }
+           
 public void actionPerformed(ActionEvent e){
      
     if(e.getSource()==b){
-        String inpstr=t2.getText(); 
+        //inpstr=t2.getText(); 
    } 
 
 }
 
-	public static void main (String[] args) throws java.lang.Exception
-	   
-	{
-         //my test
-        test t=new test();
+public void steganography_and_encryption() throws Exception
+{
+	 
+     System.out.print("Enter the message: ");
+    // String str=s.nextLine();
+     str=str.replaceAll(" ","").toLowerCase();
+     System.out.println(str);
+     str=str.replace("", " ").trim();
+     //char[] strarr=str.toCharArray();
+ //  String str1 = String.valueOf(strarr);
+     String[] strarr=str.split(" ");
+     System.out.println("your message :"+str);
+     
+ //  System.out.println(strarr.length);
+//for(int i=0;i<strarr.length;i++)
+//System.out.println(strarr[i]);
+     
 
-
-
-		//Scanner s=new Scanner(System.in);
-		//System.out.print("Enter the message: ");
-		String str=t.inpstr;//s.nextLine();
-        //ends here
-        Scanner s=new Scanner(System.in);
-        System.out.print("Enter the message: ");
-        String str=s.nextLine();
-        str=str.replaceAll(" ","").toLowerCase();
-        System.out.println(str);
-        str=str.replace("", " ").trim();
-        //char[] strarr=str.toCharArray();
-    //  String str1 = String.valueOf(strarr);
-        String[] strarr=str.split(" ");
-        System.out.println("your message :"+str);
-    //  System.out.println(strarr.length);
-//  for(int i=0;i<strarr.length;i++)
-//  System.out.println(strarr[i]);
-        
-
-        //every instance of the word is converted
-        String[] cover={
+     //every instance of the word is converted
+     String[] cover={
 "a",
 "b",
 "c",
@@ -101,11 +115,11 @@ public void actionPerformed(ActionEvent e){
 "x",
 "y",
 "z"
-        };
+     };
 
 //these are the words considered as cover message
-    String[] ukwords={
-        "accessorise",
+ String[] ukwords={
+     "accessorise",
 "accessorised",
 "accessorises",
 "accessorising",
@@ -131,131 +145,147 @@ public void actionPerformed(ActionEvent e){
 "aggrandisement",
 "agonise",
 "agonised"
-        
-    };
-    //as replaceAll tends to some buggy situations better we use another Array of String and do it
-    String ukwordsupper[]=new String[ukwords.length];
-    for(int j=0;j<ukwords.length;j++){
-        ukwordsupper[j]=ukwords[j].toUpperCase();
-        //System.out.println(ukwordsupper[j]+" ");
-    }
+     
+ };
+ //as replaceAll tends to some buggy situations better we use another Array of String and do it
+ String ukwordsupper[]=new String[ukwords.length];
+ for(int j=0;j<ukwords.length;j++){
+     ukwordsupper[j]=ukwords[j].toUpperCase();
+     //System.out.println(ukwordsupper[j]+" ");
+ }
 
-        //finding all occurances of alphabets and change them to words
-        for(int i=0;i<cover.length;i++){
-            str=str.replace(cover[i],ukwordsupper[i]);
-        }
-            //for testing
-        str=str.toLowerCase();
-            System.out.println("Cover Text is: "+str+".");
+     //finding all occurances of alphabets and change them to words
+     for(int i=0;i<cover.length;i++){
+         str=str.replace(cover[i],ukwordsupper[i]);
+     }
+         //for testing
+     str=str.toLowerCase();
+         System.out.println("Cover Text is: "+str+".");
 
-            String[] replacesplit= str.split(" ");
+         String[] replacesplit= str.split(" ");
 
 
-            //conformation from user side base on alphabet map
-            try {
-                File confirm = new File("confirm.txt");
-                FileWriter f = new FileWriter(confirm);
-        
-        
-        
-          
-            for(int i=0;i<strarr.length;i++){
-                String steganotext="'"+strarr[i]+"'"+" "+"hidden in"+" "+"'"+replacesplit[i]+"'";
-                System.out.println(steganotext);
-            f.write(steganotext+"\n");
-           // f.write("\n\n\n");
-        
-            
-        } 
-        
-        f.flush();    
-        f.close();
-    }catch (IOException iox) {
-            iox.printStackTrace();
+         //conformation from user side base on alphabet map
+         try {
+             File confirm = new File("confirm.txt");
+             FileWriter f = new FileWriter(confirm);
+     
+     
+     
+       
+         for(int i=0;i<strarr.length;i++){
+             String steganotext="'"+strarr[i]+"'"+" "+"hidden in"+" "+"'"+replacesplit[i]+"'";
+             System.out.println(steganotext);
+         f.write(steganotext+"\n");
+        // f.write("\n\n\n");
+     
+         
+     } 
+     
+     f.flush();    
+     f.close();
+ }catch (IOException iox) {
+         iox.printStackTrace();
 }
 
 
 
-            //generating file
-            try {
-          
-            File coverfile = new File("Covertext.txt");
-
-            FileWriter fw = new FileWriter(coverfile);
-         
-            fw.write(str+".");
-            fw.close();
-            if(coverfile.exists()){
-        System.out.println("Cover File Generated Sucessfully");
-         }
-        } catch (IOException iox) {
-            iox.printStackTrace();
-        }
-
-
-        //Encryption and decryption
-
-
-
-
-        String plainText = str;
-        SecretKey secKey = getSecretEncryptionKey();
-
-        byte[] cipherText = encryptText(plainText, secKey);
-
-        String decryptedText = decryptText(cipherText, secKey);
-
-         
-
-      //  System.out.println("Original Text:" + plainText);
-
-        System.out.println("AES Key (Hex Form):"+bytesToHex(secKey.getEncoded()));
-
-       // System.out.println("Encrypted Text (Hex Form):"+bytesToHex(cipherText));
-
-       // System.out.println("Descrypted Text:"+decryptedText);
-
-         String cipherstring=bytesToHex(cipherText); //i have stored in byte[] as it is unreadable
-
-
-
-// creating the cipher file
-
-
-
-            try {
-          
-            File cipherfile = new File("Cipherfile.txt");
-
-            FileWriter fw = new FileWriter(cipherfile);
-         
-            fw.write(cipherstring);
-            fw.close();
-            if(cipherfile.exists()){
-        System.out.println("CipherFile Generated Sucessfully");
-         }
-        } catch (IOException iox) {
-            iox.printStackTrace();
-        }
-
-
-
- // creating decipher file
- try {
-          
-            File decipherfile = new     File("Decipherfile.txt");
-
-            FileWriter fw = new FileWriter(decipherfile);
-         
-            fw.write(decryptedText);
-            fw.close();
-            if(decipherfile.exists()){
-        System.out.println("DecipherFile Generated Sucessfully");
-         }
-        } catch (IOException iox) {
-            iox.printStackTrace();
-        }       
+         //generating file
+         try {
        
+         File coverfile = new File("Covertext.txt");
+
+         FileWriter fw = new FileWriter(coverfile);
+      
+         fw.write(str+".");
+         fw.close();
+         if(coverfile.exists()){
+     System.out.println("Cover File Generated Sucessfully");
+      }
+     } catch (IOException iox) {
+         iox.printStackTrace();
+     }
+
+
+     //Encryption and decryption
+
+
+
+
+     String plainText = str;
+     SecretKey secKey = getSecretEncryptionKey();
+
+     byte[] cipherText = encryptText(plainText, secKey);
+
+     String decryptedText = decryptText(cipherText, secKey);
+
+      
+
+   //  System.out.println("Original Text:" + plainText);
+
+     System.out.println("AES Key (Hex Form):"+bytesToHex(secKey.getEncoded()));
+
+    // System.out.println("Encrypted Text (Hex Form):"+bytesToHex(cipherText));
+
+    // System.out.println("Descrypted Text:"+decryptedText);
+
+      String cipherstring=bytesToHex(cipherText); //i have stored in byte[] as it is unreadable
+
+
+
+//creating the cipher file
+
+
+
+         try {
+       
+         File cipherfile = new File("Cipherfile.txt");
+
+         FileWriter fw = new FileWriter(cipherfile);
+      
+         fw.write(cipherstring);
+         fw.close();
+         if(cipherfile.exists()){
+     System.out.println("CipherFile Generated Sucessfully");
+      }
+     } catch (IOException iox) {
+         iox.printStackTrace();
+     }
+
+
+
+// creating decipher file
+try {
+       
+         File decipherfile = new     File("Decipherfile.txt");
+
+         FileWriter fw = new FileWriter(decipherfile);
+      
+         fw.write(decryptedText);
+         fw.close();
+         if(decipherfile.exists()){
+     System.out.println("DecipherFile Generated Sucessfully");
+      }
+     } catch (IOException iox) {
+         iox.printStackTrace();
+     }       
+     }
+
+
+	public static void main (String[] args) throws Exception
+	   
+	{
+         //my test
+        stego t=new stego();
+
+
+
+		//Scanner s=new Scanner(System.in);
+		//System.out.print("Enter the message: ");
+//	String str=t.str;//s.nextLine();
+        //ends here
+        //Scanner s=new Scanner(System.in);
+          
 
     }
 
