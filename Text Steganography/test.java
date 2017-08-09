7dcb3bb7ef1d60b3189e314a849e267b14407efd
Bhargav Sai Gajula <bhargavsai554@gmail.com>
2017-08-09 21:05:35 +0530

    Add files via upload

--- Text Steganography/test.java
+++ Text Steganography/test.java
@@ -16,32 +16,56 @@ import java.awt.event.*;
 public class test
 {
 
-
-	public static void main (String[] args) throws java.lang.Exception
-	   
-	{
+  final static int NO_WORDS = 5;   
+    final static int NO_SENTS = 20; 
+    final static String SPACE = " ";    
+    final static String PERIOD = ".";
+    static Random r = new Random();
+ /* private static String makeSentence(){
+    Random rand = new Random();
+    int[] index = new int[6];
+    String sent = new String();
+ 
+    for(int i=0;i<noun.length;i++){
+         index[i]= rand.nextInt(5);
+    }
+    for(int i=0;i<6;i++){
+    sent = article[i] + " " + noun[i] + " " + verb[i] + " " + preposition[i] + " " + article[i] + " " + noun[i];
+    }
+    return sent;
+  }*/
+    static int rand(){
+        int ri = r.nextInt() % NO_WORDS;
+        if ( ri < 0 )
+            ri += NO_WORDS;
+        return ri;
+    }
+    public static void main (String[] args) throws java.lang.Exception
+       
+    {
          //my test
      
 
 
-		Scanner s=new Scanner(System.in);
-		System.out.print("Enter the message: ");
-		String str=s.nextLine();
-		str=str.replaceAll(" ","").toLowerCase();
-		System.out.println(str);
-		str=str.replace("", " ").trim();
-		//char[] strarr=str.toCharArray();
-	//	String str1 = String.valueOf(strarr);
-		String[] strarr=str.split(" ");
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the message: ");
+        String str=s.nextLine();
+        str=str.replaceAll(" ","").toLowerCase();
+        System.out.println(str);
+        str=str.replace("", " ").trim();
+        //char[] strarr=str.toCharArray();
+    //  String str1 = String.valueOf(strarr);
+        
 
-		System.out.println("your message :"+str);
-	//	System.out.println(strarr.length);
-//	for(int i=0;i<strarr.length;i++)
-//	System.out.println(strarr[i]);
-		
+        System.out.println("your message :"+str);
+    //  System.out.println(strarr.length);
+        String[] strarr=str.split(" ");
+ for(int i=0;i<strarr.length;i++)
+  System.out.println(strarr[i]);
+        
 
-		//every instance of the word is converted
-		String[] cover={
+        //every instance of the word is converted
+        String[] cover={
 "a",
 "b",
 "c",
@@ -68,11 +92,11 @@ public class test
 "x",
 "y",
 "z"
-		};
+        };
 
 //these are the words considered as cover message
-	String[] ukwords1={
-		"accessorise",
+    String[] ukwords1={
+        "accessorise",
 "accessorised",
 "accessorises",
 "accessorising",
@@ -98,8 +122,8 @@ public class test
 "aggrandisement",
 "agonise",
 "agonised"
-		
-	};
+        
+    };
     String[] ukwords2={
         "backpedalled",
 "backpedalling",
@@ -156,37 +180,55 @@ public class test
 "capitalise",
 "capitalised"
     };
-	//as replaceAll tends to some buggy situations better we use another Array of String and do it
-	String ukwordsupper1[]=new String[ukwords1.length];
+    //as replaceAll tends to some buggy situations better we use another Array of String and do it
+    String ukwordsupper1[]=new String[ukwords1.length];
     String ukwordsupper2[]=new String[ukwords2.length];
     String ukwordsupper3[]=new String[ukwords3.length];
-	for(int j=0;j<ukwords1.length;j++){
-		ukwordsupper1[j]=ukwords1[j].toUpperCase();
+    for(int j=0;j<ukwords1.length;j++){
+        ukwordsupper1[j]=ukwords1[j].toUpperCase();
         ukwordsupper2[j]=ukwords2[j].toUpperCase();
         ukwordsupper3[j]=ukwords3[j].toUpperCase();
-		//System.out.println(ukwordsupper[j]+" ");
-	}
+        //System.out.println(ukwordsupper[j]+" ");
+    }
 
-		//finding all occurances of alphabets and change them to words
-		for(int i=0;i<cover.length;i+=2){
-   
-          
-
-			str=str.replaceFirst(cover[i],ukwordsupper1[i]);
-           // i++;
-               // str=str.replace(cover[i],ukwordsupper2[i]);
-
-		}
-       for(int i=1;i<cover.length;i+=2){
-   
-          
-
-            str=str.replaceFirst(cover[i],ukwordsupper2[i]);
+        //finding all occurances of alphabets and change them to words
         
-               
+        for(int i=0;i<cover.length;i++){
+   
+          
+
+               str=str.replace(cover[i],ukwordsupper1[i]);
 
         }
-     for(int i=2;i<cover.length;i+=3){
+
+        str=str.toLowerCase();
+        System.out.println(str);
+    
+       String[] temp=str.split(" ");
+ /*for(int i=0;i<temp.length;i++)
+  System.out.println("The"+" "+temp[i]+" "+"is something");
+*/
+String article[] = { "the", "a", "one", "some", "any" };
+        String noun[] = { "boy", "girl", "dog", "town", "car" };
+        String verb[] = { "drove", "jumped", "ran", "walked", "skipped" };
+        String preposition[] = { "to", "from", "over", "under", "on" };
+
+        String sentence,temporary="";
+        for (int i = 0; i < temp.length; i++){
+            sentence = article[rand()];
+            char c = sentence.charAt(0);
+            sentence = sentence.replace( c, Character.toUpperCase(c) );
+            sentence += SPACE + temp[i] + SPACE;
+            sentence += SPACE + noun[rand()] + SPACE;
+            sentence += (verb[rand()] + SPACE + preposition[rand()]);
+            sentence += (SPACE + article[rand()] + SPACE + noun[rand()]);
+            sentence += PERIOD;
+            temporary+=sentence+"\n";
+            System.out.println(sentence);
+            sentence = "";
+        }  
+        
+    /* for(int i=2;i<cover.length;i+=3){
    
           
 
@@ -194,59 +236,59 @@ public class test
         
                
 
-        }
-			//for testing
-		str=str.toLowerCase();
-			System.out.println("Cover Text is: "+str+".");
+        }*/
+            //for testing
+        /*str=str.toLowerCase();
+            System.out.println("Cover Text is: "+str+".");
 
-			String[] replacesplit= str.split(" ");
-
-
-			//conformation from user side base on alphabet map
-			try {
-				File confirm = new File("confirm.txt");
-				FileWriter f = new FileWriter(confirm);
-		
-		 
-		
+            String[] replacesplit= str.split(" ");
+*/
+           
+            //confirmation from user side base on alphabet map
+       /*     try {
+                File confirm = new File("confirm.txt");
+                FileWriter f = new FileWriter(confirm);
+        
+         
+        
           
             for(int i=0;i<strarr.length;i++){
-				String steganotext="'"+strarr[i]+"'"+" "+"hidden in"+" "+"'"+replacesplit[i]+"'";
+                String steganotext="'"+strarr[i]+"'"+" "+"hidden in"+" "+"'"+replacesplit[i]+"'";
                 //System.out.println(str1);
-				System.out.println(steganotext);
+                System.out.println(steganotext);
                
                 f.write(steganotext+"\n");
 
            // f.write("\n\n\n");
         
-			
+            
         } 
         
         f.flush();    
-       	f.close();
-	}catch (IOException iox) {
+        f.close();
+    }catch (IOException iox) {
             iox.printStackTrace();
 }
+*/
 
 
-
-			//generating file
-			try {
+            //generating file
+         try {
           
             File coverfile = new File("Covertext.txt");
 
             FileWriter fw = new FileWriter(coverfile);
          
-            fw.write(str+".");
+            fw.write(temporary);
             fw.close();
-			if(coverfile.exists()){
-    	System.out.println("Cover File Generated Sucessfully");
-   		 }
+            if(coverfile.exists()){
+        System.out.println("Cover File Generated Sucessfully");
+         }
         } catch (IOException iox) {
             iox.printStackTrace();
         }
 
-
+/*
         //Encryption and decryption
 
 
@@ -277,7 +319,7 @@ public class test
 
 
 
-         	try {
+            try {
           
             File cipherfile = new File("Cipherfile.txt");
 
@@ -285,9 +327,9 @@ public class test
          
             fw.write(cipherstring);
             fw.close();
-			if(cipherfile.exists()){
-    	System.out.println("CipherFile Generated Sucessfully");
-   		 }
+            if(cipherfile.exists()){
+        System.out.println("CipherFile Generated Sucessfully");
+         }
         } catch (IOException iox) {
             iox.printStackTrace();
         }
@@ -303,9 +345,9 @@ public class test
          
             fw.write(decryptedText);
             fw.close();
-			if(decipherfile.exists()){
-    	System.out.println("DecipherFile Generated Sucessfully");
-   		 }
+            if(decipherfile.exists()){
+        System.out.println("DecipherFile Generated Sucessfully");
+         }
         } catch (IOException iox) {
             iox.printStackTrace();
         }       
@@ -354,5 +396,6 @@ public class test
         return DatatypeConverter.printHexBinary(hash);
     
 
-	}
+    }*/
+}
 }
